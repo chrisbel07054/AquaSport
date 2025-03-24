6394fbb57db5acf7153f0b0df82ded11c3951bab
chris07054 <bricenobricenoca@uvm.edu.ve>
2025-03-24 16:05:44 -0400

    Rutas de auth y usuario

---
+++ servidor/routes/usuario.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const UsuarioController = require('../controllers/UsuarioController');
+const { authenticate, authorize } = require('../middleware/authMiddleware')
+
+
+router.get('/', authenticate, authorize(['admin']), UsuarioController.obtenerTodosLosUsuarios);
+// Rutas para obtener torneos y testimonios del usuario
+router.get('/torneos/:id', authenticate, UsuarioController.obtenerTorneosUsuario);
+router.get('/testimonios/:id', authenticate, UsuarioController.obtenerTestimoniosUsuario);
+
+// Ruta para actualizar perfil de usuario
+router.put('/:id', authenticate, UsuarioController.actualizarPerfilUsuario);
+
+module.exports = router;
