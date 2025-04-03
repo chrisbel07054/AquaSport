85b7814f61968379f64387c9a8219af864708fc8
mendezchacinga <gustavo2004mendez@gmail.com>
2025-04-02 21:12:52 -0400

    Endpoints en las funciones axios

--- cliente/src/service/apiService.js
+++ cliente/src/service/apiService.js
@@ -15,31 +15,39 @@ const request = async (url, method = 'GET', data = null, token = null) => {
 
 
 export const api = {
-    auth: {
-        register: (data) => request('/auth/register', 'POST', data),
-        login: (data) => request('/auth/login', 'POST', data),
-    },
-    torneo: {
-        create: (data, token) => request('/torneo', 'POST', data, token),
-        getAll: (token) => request('/torneo', 'GET', null, token),
-        getAllActivos: () => request('/torneo/activos'),
-        getById: (id) => request('/torneo/' + id),
-        inscribirse: (torneoId, usuarioId, token) => request(`/torneo/inscripcion/${torneoId}`, 'POST', {usuarioId}, token),
-        changeStatus: (torneoId, data, token) => request(`/torneo/cambiar-estado/${torneoId}`, 'PUT', data, token),
-        updateTorneo: (torneoId, data, token) => request(`/torneo/${torneoId}`, 'PUT', data, token),
-    },
-    testimonio: {
-        create: (data, token) => request('/testimonio', 'POST', data, token),
-        getAll: () => request('/testimonio', 'GET'),
-    },
-    usuario: {
-        getAllUsuarios: (token) => request('/usuario', 'GET', null, token),
-        getTorneos: (userId, token) => request(`/usuario/torneos/${userId}`, 'GET', null, token),
-        getTestimonios: (userId, token) => request(`/usuario/testimonios/${userId}`, 'GET', null, token),
-        updateProfile: (userId, data, token) => request(`/usuario/${userId}`, 'PUT', data, token),
-    },
-    contacts: {
-        create: (data) => request('/contacts', 'POST', data),
-        getAll: () => request('/contacts', 'GET'),
-    }
+  auth: {
+    register: (data) => request("/auth/register", "POST", data),
+    login: (data) => request("/auth/login", "POST", data)
+  },
+  torneo: {
+    create: (data, token) => request("/torneo", "POST", data, token),
+    getAll: (token) => request("/torneo", "GET", null, token),
+    getAllActivos: () => request("/torneo/activos"),
+    getAllFinalizados: () => request("/torneo/finalizados"),
+    getById: (id) => request("/torneo/" + id),
+    inscribirse: (torneoId, usuarioId, token) =>
+      request(`/torneo/inscripcion/${torneoId}`, "POST", { usuarioId }, token),
+    changeStatus: (torneoId, data, token) =>
+      request(`/torneo/cambiar-estado/${torneoId}`, "PUT", data, token),
+    updateTorneo: (torneoId, data, token) =>
+      request(`/torneo/${torneoId}`, "PUT", data, token)
+  },
+  testimonio: {
+    create: (data, token) => request("/testimonio", "POST", data, token),
+    getAll: () => request("/testimonio", "GET")
+  },
+  usuario: {
+    getAllUsuarios: (token) => request("/usuario", "GET", null, token),
+    getUserById: (usuarioId) => request(`/usuario/${usuarioId}`),
+    getTorneos: (userId, token) =>
+      request(`/usuario/torneos/${userId}`, "GET", null, token),
+    getTestimonios: (userId, token) =>
+      request(`/usuario/testimonios/${userId}`, "GET", null, token),
+    updateProfile: (userId, data, token) =>
+      request(`/usuario/${userId}`, "PUT", data, token)
+  },
+  contacts: {
+    create: (data) => request("/contacts", "POST", data),
+    getAll: () => request("/contacts", "GET")
+  }
 };
